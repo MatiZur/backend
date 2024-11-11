f3d0a349174d1800bb733dbf0fb1ddbf310407ed
MatiZur <matyzur@gmail.com>
2024-11-10 21:59:02 -0300

    añadido coso de port

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 // importa la libreria express
 const express = require('express');
 
+// variable del puerto con valor 3000
+const port = process.env.PORT || 3000;
+
 // crea el objeto app
 const app = express();
 
@@ -10,5 +13,5 @@ app.get('/', (req, res) => {
   res.send('Hello world');
 });
 
-// el server escucha en el puerto 3000
-app.listen(3000);
+// el server escucha en el puerto port (3000)
+app.listen(port);
